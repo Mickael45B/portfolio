4dfa72081cd50d07c58ba49dd7009dbffb5816a6
Michael professeur <michaloiret@hotmail.fr>
2025-02-16 14:28:11 +0100

    texte a gauche, image a droite

--- Frontend/src/pages/Home/index.tsx
+++ Frontend/src/pages/Home/index.tsx
@@ -4,12 +4,14 @@
 function Home() {
   return (
     <>
-      <main className="flex flex-row items-center w-full h-full">
+      <main className="flex flex-row items-center w-full h-full ">
         
-        <div className="flex flex-col basis-3/5 w-full h-full justify-center items-center">
-          <p>Brissot Michael, developpeur full stack.</p>
-          <p>"Un backend solide pour des projets qui tiennent la route."</p>
-          <p className="w-4xl leading-7">Passionné par l’optimisation et la logique métier, je développe des applications robustes et évolutives. Autodidacte depuis 10 ans, j'aime partager mes connaissances et rendre le développement accessible aux nouvelles générations.</p>
+        <div className="flex flex-col basis-3/5 w-full h-full justify-around items-center p-28">
+          <p className="text-4xl">Brissot Michael, developpeur full stack.</p>
+          <div className="flex flex-col justify-center items-center">
+            <p className="leading-9 mb-8">"Un backend solide pour des projets qui tiennent la route."</p>
+            <p className="w-4xl leading-7 flex text-center">Passionné par l’optimisation et la logique métier, je développe des applications robustes et évolutives. Autodidacte depuis 10 ans, j'aime partager mes connaissances et rendre le développement accessible aux nouvelles générations.</p>
+          </div>
         </div>
         <div className="basis-2/5 w-full h-full bg-[url(../partie-droite.jpg)] bg-cover bg-center">
         </div>
